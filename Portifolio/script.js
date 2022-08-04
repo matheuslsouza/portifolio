75832709981cc22b718e46eb94a6dd9788e85bd4
Matheus Lourenço de Souza <107487181+matheuslsouza@users.noreply.github.com>
2022-08-04 10:22:11 -0300

    Add files via upload

--- Portifolio/script.js
+++ Portifolio/script.js
@@ -1,2 +1,32 @@
  document.querySelector (".hamburguer").addEventListener("click", ()=>
     document.querySelector(".container").classList.toggle("show-menu"));
+
+    document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+    document.querySelector("#js").addEventListener("change", atualizarPreco)
+    document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+    document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+    document.querySelector("#prazo").addEventListener("change", function() {
+      const prazo = document.querySelector("#prazo").value
+      document.querySelector("label[for=prazo]").innerHTML= `Prazo: ${prazo} semanas`
+      atualizarPreco()
+   })
+
+    function atualizarPreco(){
+       const qtde = document.querySelector("#qtde").value
+       const temJS = document.querySelector("#js").checked
+       const incluiLayout = document.querySelector("#layout-sim").checked
+       const prazo = document.querySelector("#prazo").value
+
+
+       let preco = qtde * 200;
+       if(temJS) {
+         preco = preco + (preco * 0.20)
+       }
+       if(incluiLayout){
+         preco = preco + 600
+       }
+       let taxaUergencia = 1 - prazo*0.1
+       preco = preco + (preco*taxaUergencia)
+       document.querySelector("#preco").innerHTML = `R$  ${preco.toFixed(2)}`
+    }
+
